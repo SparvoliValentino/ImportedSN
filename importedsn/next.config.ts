0a82f7a25b9d48fe6d6a273b1a8cadcb1a277bf7
Valensparvoli <sparvolivalentino@gmail.com>
2025-04-17 10:53:28 -0300

    PAW ingresado y funcionando

--- importedsn/next.config.ts
+++ importedsn/next.config.ts
@@ -1,15 +1,22 @@
-import type { NextConfig } from "next";
+import { RemotePattern } from "next/dist/shared/lib/image-config";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "drive.google.com",
-      },
-    ],
+const remotePatterns: RemotePattern[] = [
+  {
+    protocol: "https",
+    hostname: "drive.google.com",
   },
+];
+
+const nextConfig = {
+  images: {
+    remotePatterns,
+  },
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
---
+++ importedsn/src/app/head.tsx
@@ -0,0 +1,14 @@
+// app/head.tsx
+export default function Head() {
+    return (
+      <>
+        <title>MOND</title>
+        <meta name="description" content="MOND oficial website" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </>
+    );
+  }
+  
